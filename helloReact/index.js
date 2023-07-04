c277a4c9bb675836555ade6c740b798d3f1a3a43
Abdul Shahid Mohammed <shahidabdul235@gmail.com>
2023-07-03 21:10:41 -0400

    July 3 First react project

---
+++ helloReact/index.js
@@ -0,0 +1,32 @@
+// const details = (
+//   <div>
+//     <h1>Hello Shahid!</h1>
+//     <h2>I am learning react.</h2>
+//   </div>
+// );
+
+// ReactDOM.render(details, document.getElementById("root"));
+
+// console.log(details);
+// ReactDOM.render(<h2>I am learning react</h2>, document.getElementById("root"));
+
+// ReactDOM.render(
+//   <ul>
+//     <li>First</li>
+//     <li>Second</li>
+//     <li>Third</li>
+//   </ul>,
+//   document.getElementById("root")
+// );
+
+const navi = (
+  <nav>
+    <h1>Hello, Shahid!</h1>
+    <ul>
+      <li>First</li>
+      <li>Second</li>
+    </ul>
+  </nav>
+);
+
+ReactDOM.render(navi, document.getElementById("root"));
